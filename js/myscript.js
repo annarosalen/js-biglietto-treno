5ddc57153ca5c8294b0c09f9c9f2092a20f7a7d0
Anna <rosalenanna@gmail.com>
2020-10-20 16:41:41 +0200

    condizioni

--- js/myscript.js
+++ js/myscript.js
@@ -1,25 +1,36 @@
 // ESERCIZIO: calcolo del prezzo del biglietto del treno
 // Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
 
-
+var prezzoalKm = 0.21;
 
 // input: utente inserisce età
-var etàUtente = parseInt( prompt("Inserisci la tua età"));
+var etaUtente = parseInt(prompt("Inserisci la tua età"));
 
+console.log("età utente", etaUtente);
 
 // input:utente inserisce km da percorrere
-var kmUtente = parseInt( prompt("Quanti km vuoi percorrere?"));
+var kmUtente = parseInt(prompt("Quanti km vuoi percorrere?"));
 
-
+console.log("km da percorrere", kmUtente);
 
 
 // calcolo prezzo in base all'età
 
-//se l'utente è minorenne sconto del 20% (80%)
+//se l'utente è minorenne (80%)
+if (etaUtente<18) {
+  // 20% di sconto
+  console.log ((prezzoalKm * kmUtente)*(80/100));
+  // se utente è over 65  (60%)
+}else if (etaUtente>65){
+  // sconto del 40%
+  console.log ((prezzoalKm * kmUtente)*(60/100));
+  // se età utente tra i 18 e i 65 (100%)
+}else (etaUtente>=18)&&(etaUtente<=65)
+  // prezzo pieno
+  console.log (prezzoalKm * kmUtente);
 
-// se età utente tra i 18 e i 65 prezzo pieno (100%)
 
-// se utente è over 65 sconto del 40% (60%)
+
 
 
 
