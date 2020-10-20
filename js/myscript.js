4966a86eed9092c6b4c25a3482bb0634142da63c
Anna <rosalenanna@gmail.com>
2020-10-20 16:01:02 +0200

    impostazione esercizio

---
+++ js/myscript.js
@@ -0,0 +1,26 @@
+// ESERCIZIO: calcolo del prezzo del biglietto del treno
+// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+
+
+
+// input: utente inserisce età
+var etàUtente = parseInt( prompt("Inserisci la tua età"));
+
+
+// input:utente inserisce km da percorrere
+var kmUtente = parseInt( prompt("Quanti km vuoi percorrere?"));
+
+
+
+
+// calcolo prezzo in base all'età
+
+//se l'utente è minorenne sconto del 20% (80%)
+
+// se età utente tra i 18 e i 65 prezzo pieno (100%)
+
+// se utente è over 65 sconto del 40% (60%)
+
+
+
+// OUTPUT: prezzo finale
